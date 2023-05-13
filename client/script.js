6e195ab6bda7f23f6d6dbe0b96971afcdeacb068
Gyinae <dev.gyinae@gmail.com>
2023-05-13 11:19:21 +0000

    server updated url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
   // fetch response from server. Bot's response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://mariaai.onrender.com", {
     method: "POST",
     headers: {
       "content-type": "application/json",
